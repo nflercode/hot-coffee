c4c33cfae75eb3c88a9e056fc3dd42d497e52bd3
Isaac <isaac.pieber.3836@student.uu.se>
2020-08-05 23:01:08 +0200

    en endring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 function App() {
-  // const data = fetch('http://localhost:3000/arguments').then(data => data.json(), (err) => console.error(err)).then(a => console.log(JSON.stringify(a)));
+  fetch('https://api.nfler.se/arguments').then(data => data.json(), (err) => console.error(err)).then(a => console.log(JSON.stringify(a)));
   return (
     <div className="App">
       <header className="App-header">
         <h1>nfler - arguing is healthy</h1>
-        <h2>Some new info</h2>
-        <p>Interesting ...</p>
+
+        <h1>Hej Sandra! Snart syns vi ;)</h1>
       </header>
     </div>
   );
