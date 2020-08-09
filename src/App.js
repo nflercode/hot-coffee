1e9ca7dd450f914c5c675b5bed93e2cc18cbf24b
Isaac <isaac.pieber.3836@student.uu.se>
2020-08-09 22:30:01 +0200

    damn boi

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function App() {
   fetch('https://api.nfler.se/arguments').then(data => data.json(), (err) => console.error(err)).then(a => console.log(JSON.stringify(a)));
+  fetch('https://api.nfler.se/fun').then(data => data.json(), (err) => console.error(err)).then(a => console.log(JSON.stringify(a)));
   return (
     <div className="App">
       <header className="App-header">
         <h1>nfler - arguing is healthy</h1>
-
-        <h1>Hej Sandra! Snart syns vi ;)</h1>
+        <span>The one and only</span>
       </header>
     </div>
   );
