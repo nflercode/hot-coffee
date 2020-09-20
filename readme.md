60c22f3cf291dd9e78d328f14b7eb1b7e473bdfb
Philip Koivunen <philip.koivunen@outlook.com>
2020-09-20 22:37:06 +0200

    added a readme a navigationbar and moved not found to its own page and made some other styling improvements

---
+++ readme.md
@@ -0,0 +1,5 @@
+#hot-coffee
+Frontend for NFLER
+
+Run npm install to install nessessary pacakges.
+Run npm start - to start the webbaplication
--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import Routes from "./components/router/scripts/router";
 
 export default function App() {
   return (
-    <div>
-      <h1>NFLER (not Netflix)</h1>
-      <h2>Next lvl deployment</h2>
-      <h3>Super next level</h3>
-      <Routes />
-    </div>
+    <Routes />
   );
 }
