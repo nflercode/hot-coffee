ab6e7f4bda070b9d65637d6f3f52cb326d53214c
Philip Koivunen <philip.koivunen@outlook.com>
2021-08-05 00:15:40 +0200

    redesigned the join page to mimic the design of create page

--- src/pages/join-page/join-page.js
+++ src/pages/join-page/join-page.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router';
-import { Link } from 'react-router-dom';
 import tableService from '../../services/table-service';
 import refreshTokenStorage from '../../storage/refresh-token-storage';
+import {Button} from '../../components/button/button';
+import { Input } from '../../components/input-field/input';
+import playerService from '../../services/player-service';
+import './join-page.css';
 
 const JoinPage = () => {
     const dispatch = useDispatch();
@@ -38,32 +41,55 @@ const JoinPage = () => {
         joinTable();
     }
 
+    
+    const handleCopy = () => {
+        const copyText = document.querySelector("#copy-input").querySelector("input");
+
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+
+        document.execCommand("copy");
+    };
+
+
+    if(authState.authToken.token) return (
+        <main className="join-page-main">
+            <p>Det ser ut som att du är med i ett bord redan</p>
+                <div className="start-page-leave-or-join">
+                  <Button  onClick={() => history.push('/lobby')}>Till lobby</Button>
+                  Eller
+                  <Button theme="negative" onClick={handleLeaveTable}>Lämna</Button>
+                </div>
+        </main>
+    );
+    
+    function handleLeaveTable() {
+        async function leave() {
+          await playerService.deletePlayer(authState.authToken.token);
+          refreshTokenStorage.deleteRefreshToken();
+          dispatch({ type: "PLAYER_LEFT_TABLE" });
+          history.push('/');
+        }
+    
+        leave();
+      }
     const annanOrAndra = (tableState.players || []).length === 1 ? 'annan' : 'andra';
-    return authState.authToken.token ? renderAlreadyMember(tableState) : (
-        <div>
-            <header>
-                <span>url: {`${window.origin}/join/${tableState.invitationToken}`}</span>
-            </header>
-            <main>
-                <div>
-                    <span>{(tableState.players || []).length} {annanOrAndra} sitter vid det här bordet</span>
-                </div>
-                <div>
-                    <span>Ange alias:</span>
-                    <input type="text" value={playerName} onChange={(e) => setPlayerName(e.target.value)} />
-                </div>
-                <div>
-                    <button onClick={handleJoinTableButtonClick}>Anslut till bord</button>
-                </div>
-            </main>
-        </div>
+    return (
+        <main className="join-page-main">
+            <h2>Anslut till bord</h2>
+            <div className="join-page-main-connect-url">
+                <Input id="copy-input" label="Delbar länk" type="text" isReadOnly value={`${window.origin}/join/${tableState.invitationToken}`}/>
+                <Button onClick={handleCopy}>Copy</Button>
+            </div>
+            <Input label="Alias" type="text" value={playerName} onDebouncedChange={(val) => setPlayerName(val)} />
+            <div>
+                <span>{(tableState.players || []).length} {annanOrAndra} sitter vid det här bordet</span>
+            </div>
+            <div className="join-page-done-btn">
+                <Button block onClick={handleJoinTableButtonClick}>Anslut till bord</Button>
+            </div>
+        </main>
     );
 };
-
-function renderAlreadyMember(table) {
-    return (
-        <div>Du är redan medlem i ett bord. Gå till <Link to="/lobby">lobbyn</Link> och lämna om du vill delta i detta bord istället.</div>
-    );
-}
 
 export default JoinPage;
