f05e23ba71766b2c9d9436fb11711220495f2f83
Isaac Pieber <isaacpieber96@gmail.com>
2021-08-06 01:09:13 +0200

    will it work?

--- src/components/socket/socket.js
+++ src/components/socket/socket.js
@@ -6,6 +6,7 @@ import { POT_REQUEST_FETCHED } from '../../store/reducers/pot-request';
 
 function setUpMarkerWorld(authToken) {
   const socket = io(`${process.env.REACT_APP_MARKERWORLD_HOST}`, {
+    path: '/markerworld/socket.io',
     auth: {
       token: authToken
     }
@@ -16,6 +17,7 @@ function setUpMarkerWorld(authToken) {
 
 function setUpChippie(authToken) {
   const socket = io(`${process.env.REACT_APP_CHIPPIE_HOST}`, {
+    path: '/chippie/socket.io',
     auth: {
       token: authToken
     }
