cb3b0d299c005677e683dbf6fc59d08408190ed0
Isaac Pieber <isaacpieber96@gmail.com>
2021-08-05 22:05:53 +0200

    added file

---
+++ src/store/reducers/pot-request.js
@@ -0,0 +1,10 @@
+export const POT_REQUEST_FETCHED = "POT_REQUEST_FETCHED";
+
+export const potRequest = (state = {}, action) => {
+    switch(action.type) {
+        case POT_REQUEST_FETCHED:
+            return action.potRequest;
+        default: 
+            return state;
+    }
+};
