89a5be3cd8390ccd83295641734aa987e62668b2
Philip Koivunen <philip.koivunen@outlook.com>
2021-08-16 17:50:59 +0200

    added logotyp with svg version and animated it

--- src/pages/start-page/logotype.js
+++ src/pages/start-page/logotype.js
@@ -1,4 +1,44 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Logo } from './logo-state-1';
+import { LogoStateTwo } from './logo-state-2';
+import { LogoStateThree } from './logo-state-3';
 
-export const LogoType = () => (<h1><div className="logotype"/>
-</h1>);
+export const LogoType = () => {
+    const [imageToShow, setImageToShow] = useState(1);
+    const isLooping = useRef();
+   const randomNumber = () => Math.floor(Math.random() * (20000 - 5000) + 5000);
+
+   const doFlicker = () => {
+       setImageToShow(.4);
+       isLooping.current = true;
+       setTimeout(() => { 
+           setImageToShow(1);
+           setTimeout(() => {
+               setImageToShow(.1); 
+               setTimeout(() => {
+                    isLooping.current = false;
+                   setImageToShow(1);
+                   doFlickerAfterTimeout();
+               }, 100);
+           }, 250);
+       }, 200);
+   };
+
+   const doFlickerAfterTimeout = () => {
+       setTimeout(() => {
+           if(!isLooping.current)doFlicker();
+       }, randomNumber());
+   };
+
+   useEffect(() => {
+       doFlickerAfterTimeout();
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, []);
+        
+
+    return (
+    <h1>
+        <Logo className={`logotype ${imageToShow === 0.1 ? 'logotype-gray-scale': ''}`} style={{opacity: imageToShow}} />    
+    </h1>
+    );
+};
