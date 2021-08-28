5376eebb6c4116d736d368a3d5396529e8da0c7a
Philip Koivunen <philip.koivunen@outlook.com>
2021-08-28 21:33:33 +0200

    ran prettier on all jsx files

--- src/components/copy-link/copy-link.jsx
+++ src/components/copy-link/copy-link.jsx
@@ -1,41 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 //import './copy-link.css';
-import { Button } from '../../components/button/button';
-import { Input } from '../../components/input-field/input';
-import { DialogsContext } from '../../components/dialogs/dialogs-context';
-import './copy-link.css';
+import { Button } from "../../components/button/button";
+import { Input } from "../../components/input-field/input";
+import { DialogsContext } from "../../components/dialogs/dialogs-context";
+import "./copy-link.css";
 
 export const CopyLink = ({ invitationToken }) => {
-  const dialogerinos = useContext(DialogsContext);
-  const handleCopy = () => {
-    const copyText = document
-      .querySelector('#copy-input')
-      .querySelector('input');
+    const dialogerinos = useContext(DialogsContext);
+    const handleCopy = () => {
+        const copyText = document
+            .querySelector("#copy-input")
+            .querySelector("input");
 
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
 
-    document.execCommand('copy');
+        document.execCommand("copy");
 
-    dialogerinos.onShowDialog({
-      type: 'ALERT',
-      title: 'Kopierat',
-      icon: 'fa-copy'
-    });
-  };
+        dialogerinos.onShowDialog({
+            type: "ALERT",
+            title: "Kopierat",
+            icon: "fa-copy",
+        });
+    };
 
-  return (
-    <div className='copy-link-container'>
-      <Input
-        id='copy-input'
-        label='Delbar länk'
-        type='text'
-        isReadOnly
-        value={`${window.origin}/join/${invitationToken}`}
-      />
-      <Button onClick={handleCopy}>
-        <i class='fas fa-copy'></i>
-      </Button>
-    </div>
-  );
+    return (
+        <div className="copy-link-container">
+            <Input
+                id="copy-input"
+                label="Delbar länk"
+                type="text"
+                isReadOnly
+                value={`${window.origin}/join/${invitationToken}`}
+            />
+            <Button onClick={handleCopy}>
+                <i class="fas fa-copy"></i>
+            </Button>
+        </div>
+    );
 };
--- src/components/player/player.jsx
+++ src/components/player/player.jsx
@@ -1,62 +1,62 @@
-import React, { useState } from 'react';
-import LazyLoad from 'react-lazyload';
-import './player.css';
-const imageHostBaseUrl = 'https://image.mychips.online/avatars';
+import React, { useState } from "react";
+import LazyLoad from "react-lazyload";
+import "./player.css";
+const imageHostBaseUrl = "https://image.mychips.online/avatars";
 
 export const Player = ({ playerParticipant }) => {
-  const [showCash, setShowCash] = useState(false);
+    const [showCash, setShowCash] = useState(false);
 
-  const onClick = () => {
-    if (showCash) {
-      setShowCash(false);
-      return;
-    }
+    const onClick = () => {
+        if (showCash) {
+            setShowCash(false);
+            return;
+        }
 
-    setShowCash(true);
-  };
+        setShowCash(true);
+    };
 
-  return (
-    <div
-      onClick={onClick}
-      className={`player${
-        playerParticipant.isCurrentTurn ? ' player-active' : ''
-      }`}
-    >
-      <div className='player-badge'>
-        {playerParticipant.isWorst && !playerParticipant.isBest && (
-          <span>
-            <i className='fas fa-trash fc-grey' />
-          </span>
-        )}
-        {playerParticipant.isBest && !playerParticipant.isWorst && (
-          <span>
-            <i className='fas fa-crown fc-gold' />
-          </span>
-        )}
-      </div>
+    return (
+        <div
+            onClick={onClick}
+            className={`player${
+                playerParticipant.isCurrentTurn ? " player-active" : ""
+            }`}
+        >
+            <div className="player-badge">
+                {playerParticipant.isWorst && !playerParticipant.isBest && (
+                    <span>
+                        <i className="fas fa-trash fc-grey" />
+                    </span>
+                )}
+                {playerParticipant.isBest && !playerParticipant.isWorst && (
+                    <span>
+                        <i className="fas fa-crown fc-gold" />
+                    </span>
+                )}
+            </div>
 
-      <div className='player-avatar'>
-        <LazyLoad height={60}>
-          <img
-            width={50}
-            alt={`Avatar for ${playerParticipant.avatar.name}`}
-            src={`${imageHostBaseUrl}/${playerParticipant.avatar.imageName}`}
-          />
-        </LazyLoad>
-      </div>
-      <div className='player-name f-center'>
-        {playerParticipant.name}{' '}
-        <b>
-          {showCash ? (
-            <>
-              <br />
-              {playerParticipant.totalValue}$
-            </>
-          ) : (
-            `#${playerParticipant.turnOrder}`
-          )}
-        </b>
-      </div>
-    </div>
-  );
+            <div className="player-avatar">
+                <LazyLoad height={60}>
+                    <img
+                        width={50}
+                        alt={`Avatar for ${playerParticipant.avatar.name}`}
+                        src={`${imageHostBaseUrl}/${playerParticipant.avatar.imageName}`}
+                    />
+                </LazyLoad>
+            </div>
+            <div className="player-name f-center">
+                {playerParticipant.name}{" "}
+                <b>
+                    {showCash ? (
+                        <>
+                            <br />
+                            {playerParticipant.totalValue}$
+                        </>
+                    ) : (
+                        `#${playerParticipant.turnOrder}`
+                    )}
+                </b>
+            </div>
+        </div>
+    );
 };
